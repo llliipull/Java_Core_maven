602cd8bec4f784506a03b87e7342cbf13fe0af57
llliipull <evil92.92@gmail.com>
2022-04-29 14:06:22 +0400

    Homework 6 P.S. проблема с  RestAssured была в наличии <scope>test</scope> этой строки в dependecy rest assured, убрал и заработало

--- src/main/java/HW6/Main.java
+++ src/main/java/HW6/Main.java
@@ -1,7 +1,25 @@
 package HW6;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
 public class Main {
     public static void main(String[] args) {
-        RestAss
+        RestAssured.baseURI = "http://dataservice.accuweather.com/";
+
+        Response response = given().when().get("forecasts/v1/daily/5day/296181?apikey=hZz07lQbIOppQ2lVckyljP7cmHrTNG7q");
+
+
+        if(response.getStatusCode() == 200){
+            System.out.println("Status code is 200");
+        }
+
+        System.out.println(response.asPrettyString());
+
+
     }
 }
